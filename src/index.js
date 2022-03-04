ffbe9222bb28b790758dcd8122fb0b6641a68a1a
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-04 14:37:36 -0500

    added reactive copyright date

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const creator = "Chip's Chimney";
+
+ReactDOM.render(
+  <div>
+    <p>Created by {creator}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
